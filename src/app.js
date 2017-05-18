1138a2c0af082d749dc1667503c141e4fdf0005d
Daniel Duton <daniel@duton.lu>
2017-05-18 17:23:22 +0200

    load koa + router

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-console.log('app');
+import Koa from 'koa';
+import Router from 'koa-router';
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', function(ctx) {
+    ctx.body = 'hello';
+});
+
+app
+    .use(router.routes())
+    .use(router.allowedMethods());
+
+
+app.listen(3000);
